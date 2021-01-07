d2b66ed68d2cb5533c70b6adda63d2463f5e49be
jjf7 <jfsistemas.contacto@gmail.com>
2021-01-07 17:54:46 -0400

    second upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,301 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div class="top">
+        <span>
+          <i class="fas fa-shipping-fast"></i> Env&iacute;os gratis en compras
+          superiores a US$20.00
+        </span>
+
+        <span>
+          Tasa del dia: US$1.00 <i class="fas fa-chevron-right"></i> BsS.
+          1.090.000.000,50{" "}
+        </span>
+      </div>
+
+      <div class="wrapper">
+        <div class="wrapper-top">
+          <div class="link-menu-movil">
+            <i class="fas fa-bars"></i>
+          </div>
+          <div class="search">
+            <a>
+              <i class="fab fa-whatsapp"></i> <span>+58 412 8256644</span>
+            </a>{" "}
+            /
+            <a>
+              <i class="fas fa-envelope"></i> <span>contacto@lawanda.com</span>
+            </a>
+          </div>
+          <div class="cart-icon">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+
+        <div class="title">
+          <h1>La Wanda</h1>
+        </div>
+
+        <ul class="nav-menu">
+          <li>
+            <a>lencer&iacute;a</a>
+          </li>
+          <li>
+            <a>tops</a>
+          </li>
+          <li>
+            <a>faldas</a>
+          </li>
+          <li>
+            <a>shorts</a>
+          </li>
+          <li>
+            <a>vestidos</a>
+          </li>
+          <li>
+            <a>accesorios</a>
+          </li>
+          <li>
+            <a>sexshop</a>
+          </li>
+        </ul>
+
+        <div class="items">
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item1.jpg" alt="" />
+                <img class="hidden" src="items/item1-2.jpg" alt="" />
+              </div>
+
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item2-2.jpg" alt="" />
+                <img class="hidden" src="items/item2.jpg" alt="" />
+              </div>
+
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item3.jpg" alt="" />
+                <img class="hidden" src="items/item3-2.jpg" alt="" />
+              </div>
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item4.jpg" alt="" />
+                <img class="hidden" src="items/item4-2.jpg" alt="" />
+              </div>
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item5.jpg" alt="" />
+                <img class="hidden" src="items/item5-2.jpg" alt="" />
+              </div>
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item6.jpg" alt="" />
+                <img class="hidden" src="items/item6-2.jpg" alt="" />
+              </div>
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+
+          <div class="item">
+            <a href="detalle.html">
+              <div>
+                <img src="items/item7.jpg" alt="" />
+                <img class="hidden" src="items/item7-2.jpg" alt="" />
+              </div>
+              <span class="title">conjunto lencer&iacute;a</span>
+              <div class="score">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span>(57)</span>
+              </div>
+              <div class="price">
+                <span class="old">$59.00</span>
+                <span class="actual">$100.00</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="banner-info0">
+        <h1>NOT THE RIGHT SIZE?</h1>
+        <h1>NO PROBLEM. </h1>
+
+        <span>
+          WE OFFER 90 DAYS TO RETURN AND BE <b>FULLY REFUNDED.</b> NO QUESTIONS
+          ASKED.
+        </span>
+
+        <a href="">
+          I WANT TO LEARN MORE <i class="fas fa-chevron-right"></i>
         </a>
-      </header>
-    </div>
+      </div>
+
+      <div class="banner-info">
+        <div class="item">
+          <i class="fas fa-plane"></i> <span>FREE SHIPPING WORLDWIDE</span>
+        </div>
+
+        <div class="item">
+          <i class="fas fa-hand-holding-usd"></i>{" "}
+          <span>90 DAYS MONEY BACK GUARANTEE</span>
+        </div>
+
+        <div class="item">
+          <i class="fas fa-shield-alt"></i>{" "}
+          <span>100% SAFE & SECURE CHECKOUT</span>
+        </div>
+      </div>
+
+      <footer>
+        <div>
+          <h4>LEGAL</h4>
+          <ul>
+            <li>
+              <a>Privacy Policy</a>
+            </li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>CUSTOMER SERVICE</h4>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>NEWSLETTER</h4>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>FOLLOW US</h4>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+      </footer>
+
+      <div class="footer">
+        <div>Copyright © 2020, ioshop.link.</div>
+        <div>
+          <span>Pagos v&iacute;a</span>
+          <i class="fab fa-cc-paypal"></i>
+        </div>
+      </div>
+    </>
   );
 }
 
